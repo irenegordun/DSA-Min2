4ebdc5beed3a92d0f4ec4f19cdaf3928b4bce3b5
irenegordun <irene.gordun@estuadiantat.upc.edu>
2022-06-02 09:59:18 +0200

    Main i error

---
+++ app/src/main/java/dsa/eetac/upc/edu/minim2Irene/ErrorActivity.java
@@ -0,0 +1,28 @@
+package dsa.eetac.upc.edu.minim2Irene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+
+        ImageButton return_btn = (ImageButton) findViewById(R.id.return_btn);
+
+        return_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/dsa/eetac/upc/edu/minim2Irene/MainActivity.java
@@ -0,0 +1,40 @@
+package dsa.eetac.upc.edu.minim2Irene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView user = findViewById(R.id.userNameText);
+        Button info = findViewById(R.id.info_btn);
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = user.getText().toString();
+
+                //shared preferences:
+                SharedPreferences sharedPreferences = getSharedPreferences("userName", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("User", userName);
+                editor.commit();
+
+                Intent intent = new Intent(getApplicationContext(), FollowersActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
